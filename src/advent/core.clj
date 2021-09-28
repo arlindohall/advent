12f821c93f1aa272ce0290716fcca1f63af79209
Miller Hall <hall@hey.com>
2021-09-28 17:18:26 -0400

    Empty propject with leiningen, plus basic command parsing

---
+++ src/advent/core.clj
@@ -0,0 +1,56 @@
+(ns advent.core
+  (:gen-class))
+
+(defn help
+  "Print usage and quit."
+  [args]
+  (println "Usage: advent <command> [args...]"))
+
+(defn run
+  "Execute one day."
+  [args]
+  (println "Running day" (get args "day" "unknown")))
+
+(defn prop-error
+  [& messages]
+  (help)
+  (throw (new RuntimeException (clojure.string/join " " ["ParseError: "] messages))))
+
+(defn parse-one
+  "Parse a single command line option or error"
+  [prop val]
+  (if (.startsWith prop "--")
+      (hash-map (.substring prop 2 (.length prop)) val)
+      (prop-error "invalid property" prop)))
+
+(defn parse-props
+  "Parse the props for a command"
+  [props]
+  (if (empty? props)
+      {}
+      (if (empty? (rest props))
+          (parse-one (nth props 0) true)
+          (let [first (first props) second (second props)]
+            (if (.startsWith second "--")
+              (merge (parse-one first true) (parse-props (rest props)))
+              (merge (parse-one first second) (parse-props (rest (rest props)))))))))
+
+(defn parse-command
+  [command props]
+  (cond (.equals "help" command) [help {}]
+        (.equals "run" command) [run (parse-props props)]
+        :else [help {}]))
+
+(defn parse-args
+  "Parse arguments and quit."
+  [args]
+  (println args)
+  (if-let [[command & rest] args]
+    (parse-command command rest)
+    [help {}]))
+
+(defn -main
+  "Entry point."
+  [& args]
+  (let [[command parsed] (parse-args args)]
+    (command parsed)))
