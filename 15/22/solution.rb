9c72f6535da3848bd0a94c746cf45739b37725af
Miller Arlindo Hall <hall@hey.com>
2022-05-31 16:00:07 -0400

    Still not correct but working at least

--- 15/22/solution.rb
+++ 15/22/solution.rb
@@ -1,186 +1,202 @@
 
-GameTree = Struct.new(:player, :boss, :all_games)
 Game = Struct.new(:wizard, :boss, :turns)
-Wizard = Struct.new(:mana, :hp)
-Boss = Struct.new(:damage, :hp)
 
-Spell = Struct.new(:name, :mana, :damage, :heal, :effects)
+Wizard = Struct.new(:hp, :mana)
+Boss = Struct.new(:hp, :damage)
+
+PlayerState = Struct.new(:hp, :mana, :armor)
+OpponentState = Struct.new(:hp, :damage)
+Turn = Struct.new(:attacker, :player_state, :opponent_state, :action, :effects)
+
+Spell = Struct.new(:name, :cost, :damage, :heal, :effect)
 Effect = Struct.new(:timer, :armor, :damage, :mana)
 
-Turn = Struct.new(
-  :active_player,
-  :player_state,
-  :boss_state,
-  :effects,
-  :spell,
-  :damage,
-  :mana,
-)
 
-SpellBook = [
-  Spell.new("Magic Missile", 53, 4, 0, nil),
-  Spell.new("Drain", 73, 2, 2, nil),
-  Spell.new("Shield", 113, 0, 0, Effect.new(6, 7, 0, 0)),
-  Spell.new("Poison", 173, 3, 0, Effect.new(6, 0, 3, 0)),
-  Spell.new("Recharge", 229, 0, 0, Effect.new(5, 0, 0, 101))
-]
-
-def SpellBook.find(name)
-  SpellBook.filter{ |spell| spell.name == name }.first or raise "No spell named #{name}"
-end
-
-class Game
-  def next_turn(spell = nil)
-    if active_player == :player
-      turn(SpellBook.find(spell))
-    elsif active_player == :boss
-      turn
-    else
-      throw "Invalid active player: #{active_player}"
-    end
+class Spell
+  def player_damage
+    0
   end
 
-  def turn(spell = nil)
-    Game.new(
-      wizard,
-      boss,
-      turns.clone.push(
-        Turn.new(
-            active_player,
-            player.clone,
-            opponent.clone,
-            next_effects(spell),
-            spell,
-            effects_mana,
-            effects_damage,
-          )
-        ),
-    )
-  end
-
-  def player
-    last_turn ? last_turn.player_state : wizard
-  end
-
-  def opponent
-    last_turn ? last_turn.boss_state : boss
-  end
-
-  def effects_damage
-    return 0 if last_turn.nil?
-    last_turn.damage_effects
-  end
-
-  def effects_mana
-    return 0 if last_turn.nil? || last_turn.mana_effects.nil?
-    last_turn.mana_effects
-  end
-
-  def armor_effects
-    return 0 if last_turn.nil?
-    last_turn.armor_effects
-  end
-
-  def mana_tracker
-    last_turn ? last_turn.player_state.mana : wizard.mana
-  end
-
-  def next_mana(spell = nil)
-    mana_tracker +
-      effects_mana -
-      (spell ? spell.mana : 0)
-  end
-
-  def next_effects(spell = nil)
-    if last_turn.nil?
-      spell && spell.effects ? [spell.effects] : []
-    else
-      last_turn.next_effects(spell)
-    end
-  end
-
-  def active_player
-    if last_turn.nil?
-      :player
-    else
-      last_turn.next_player
-    end
-  end
-
-  def last_turn
-    turns.last
+  def boss_damage
+    damage
   end
 
   def show
-    turns.map do |turn|
-      turn.show
-    end.join("\n\n")
+    "Player casts #{name}"
+  end
+end
+
+class Attack
+  def initialize damage
+    @damage = damage
+  end
+
+  def player_damage
+    @damage
+  end
+
+  def boss_damage
+    0
+  end
+
+  def show
+    "Boss attacks for #{@damage}"
+  end
+end
+
+class SpellBook
+  def book
+    @spells ||= [
+      Spell.new("Magic Missile", 53, 4, 0, nil),
+      Spell.new("Drain", 73, 2, 2, nil),
+      Spell.new("Shield", 113, 0, 0, Effect.new(6, 7, 0, 0)),
+      Spell.new("Poison", 173, 3, 0, Effect.new(6, 0, 3, 0)),
+      Spell.new("Recharge", 229, 0, 0, Effect.new(5, 0, 0, 101))
+    ].map do |spell|
+      [spell.name, spell]
+    end.to_h
+  end
+
+  def find(name)
+    book[name] or raise "No spell named #{name}"
   end
 end
 
 class Turn
-  def next_effects(spell)
-    if spell && spell.effects
-      updated_effects.push(spell.effects)
-    else
-      updated_effects
-    end
+  def player_dead?
+    next_player_state.hp <= 0
   end
 
-  def updated_effects
-    @updated_effects ||= effects.clone.map do |effect|
-      effect.clone
-    end.map do |effect|
-      effect.tap{ |e| e.timer -= 1 }
-    end.filter do |effect|
-      effect.timer >= 0
-    end
+  def boss_dead?
+    next_boss_state.hp <= 0
   end
 
-  def mana_effects
-    effects.map(&:mana).reject(&:nil?).sum
+  def next_boss_state
+    OpponentState.new(
+      opponent_state.hp - action.boss_damage - effects.map(&:damage).reject(&:nil?).sum,
+      opponent_state.damage
+    )
   end
 
-  def damage_effects
-    effects.map(&:damage).reject(&:nil?).sum
-  end
-
-  def armor_effects
-    effects.map(&:armor).reject(&:nil?).sum
-  end
-
-  def next_player
-    active_player == :player ? :boss : :player
+  def next_player_state
+    PlayerState.new(
+      player_state.hp + action.player_damage,
+      player_state.mana + effects.map(&:mana).sum,
+      player_state.armor + effects.map(&:armor).sum,
+    )
   end
 
   def show
+    "#{header}#{effects.map(&:to_s).join("\n")}#{player_dead? ? "\nBoss wins" : ''}#{boss_dead? ? "\nPlayer wins" : ''}"
+  end
+
+  def header
     <<~EOS
-    -- #{active_player == :player ? 'Player' : 'Boss'} turn --
-    - Player has #{player_state.hp} hp, #{armor_effects}, #{player_state.mana} mana
-    - Boss has #{boss_state.hp} hp
+    -- #{attacker == :player ? 'Player' : 'Boss'} turn --
+    - Player has #{player_state.hp} hit points, #{player_state.armor} armor, #{player_state.mana} mana
+    - Boss has #{opponent_state.hp} hit points
+    #{action.show}
     EOS
-    .strip + "\n" +
-    effects.map do |effect|
-      effect.to_s
-    end.join("\n")
+  end
+end
+
+class Game
+  def next_turn(spell = nil)
+    if [:lose, :win].include? state
+      self
+    else
+      if spell
+        spell = SpellBook.new.find(spell)
+      end
+      Game.new(
+        wizard,
+        boss,
+        turns.clone.push(Turn.new(
+          state,
+          player_state,
+          opponent_state,
+          spell ? spell : opponent_attack,
+          update_effects,
+        ))
+      )
+    end
+  end
+
+  def update_effects
+    if turns.empty?
+      []
+    else
+      updated = turns.last.effects.map do |effect|
+        effect.clone.tap do |e|
+          e.timer -= 1
+        end
+      end.filter do |effect|
+        effect.timer > 0
+      end
+
+      case turns.last.action
+      when Spell
+        updated.push(turns.last.action.effect) 
+      else
+        updated
+      end
+    end
+  end
+
+  def state
+    @state ||= if turns.empty?
+      :player
+    elsif turns.last.player_dead?
+      :lose
+    elsif turns.last.boss_dead?
+      :win
+    elsif turns.length % 2 == 0
+      :player
+    else
+      :opponent
+    end
+  end
+
+  def player_state
+    if turns.empty?
+      PlayerState.new(wizard.hp, wizard.mana, 0)
+    else
+      turns.last.next_player_state
+    end
+  end
+
+  def opponent_state
+    if turns.empty?
+      OpponentState.new(boss.hp, boss.damage)
+    else
+      turns.last.next_boss_state
+    end
+  end
+
+  def opponent_attack
+    Attack.new(boss.damage)
+  end
+
+  def show
+    turns.map(&:show).join("\n\n")
   end
 end
 
 class GameTree
-  def player_1
-    Wizard.new(250, 10)
+  def wizard_1
+    Wizard.new(10, 250)
   end
 
   def boss_1
-    Boss.new(8, 13)
+    Boss.new(13, 8)
   end
 
   def boss_2
-    Boss.new(8, 14)
+    Boss.new(14, 8)
   end
 
   def example_game_1
-    Game.new(player_1, boss_1, [])
+    Game.new(wizard_1, boss_1, [])
       .next_turn('Poison')
       .next_turn
       .next_turn('Magic Missile')
@@ -188,7 +204,7 @@ class GameTree
   end
 
   def example_game_2
-    Game.new(player_1, boss_2, [])
+    Game.new(wizard_1, boss_2, [])
   end
 end
 
