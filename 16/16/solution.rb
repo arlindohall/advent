f7f106fdb6ec6a8763454ed8a9a48c5e9f7289f2
Miller Arlindo Hall <hall@hey.com>
2022-06-19 22:10:13 -0400

    Problem 16/16 - solved somewhat efficiently

--- 16/16/solution.rb
+++ 16/16/solution.rb
@@ -2,7 +2,7 @@
 
 class Data
   def initialize(text)
-    @text = text
+    @text = text.chars.map(&:to_i)
   end
 
   def checksum
@@ -11,7 +11,8 @@ class Data
 
   def fill(size)
     until fulfilled?(size)
-      @text = step
+      puts "Stepping desired=#{size} size=#{@text.size}"
+      step!
     end
 
     @text = @text[...size]
@@ -23,25 +24,23 @@ class Data
     @text.length >= size
   end
 
-  def step
-    @text + "0" + flipped
+  def step!
+    @text << 0
+    flipped do |f|
+      @text << f
+    end
   end
 
   def flipped
-    reversed.chars.map do |char|
-      case char
-      when "0"
-        "1"
-      when "1"
-        "0"
-      else
-        raise "Unexpected character #{char}"
-      end
-    end.join
+    reversed do |bit|
+      yield(1-bit)
+    end
   end
 
   def reversed
-    @text.reverse
+    for i in (@text.length-2).downto(0)
+      yield(@text[i])
+    end
   end
 end
 
@@ -52,26 +51,33 @@ class Checksum
 
   def sum
     while @data.size.even?
+      puts "Stepping down size=#{@data.size}"
       halve_data
     end
-    @data
+    @data.map(&:to_s).join
   end
 
   def halve_data
-    @data = pairs.map{|pr| sum_one(pr)}.join
+    @data = pairs.map{|pr| sum_one(pr)}
   end
 
   def pairs
-    @data.chars.each_slice(2)
+    @data.each_slice(2)
   end
 
   def sum_one(pair)
-    pair.first == pair.last ? "1" : "0"
+    pair.first == pair.last ? 1 : 0
   end
 end
 
 @example = Data.new("10000").fill(20)
 @input = Data.new("11100010111110100").fill(272)
 
-# This will take too long, to create 35M of data and collapse it, but I'll try
-# @part2 = Data.new("11100010111110100").fill(35651584)
+<<-EXPLANATION
+This has yet to finish for me so I can time it using strings.
+But with this implementation using lists, it's maybe 30 seconds.
+
+That's because of the string cache limit, and because I used blocks/yield
+to reverse the arrays instead of map/reverse which would copy them.
+EXPLANATION
+@part2 = Data.new("11100010111110100").fill(35651584)
