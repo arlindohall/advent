3b9a50158067095b1485f61fb771e99ba23c3d4b
Miller Arlindo Hall <hall@hey.com>
2022-07-11 10:11:11 -0400

    Impossible to fully simulate, just simulate the part after 0

--- 17/17/solution.rb
+++ 17/17/solution.rb
@@ -6,16 +6,17 @@ class SpinLock
     @i = 0
   end
 
+  def dup
+    self.class.new(@steps)
+  end
+
   def solve
+    [dup.part1, dup.part2]
+  end
+
+  def part1
     2017.times { cycle }
-    part1 = find(2017)
-
-    return [part1]
-    @i, @buffer = 0, [0]
-    50000000.times { cycle }
-    part2 = find(0)
-
-    [part1, part2]
+    find(2017)
   end
 
   def find(n)
@@ -38,4 +39,25 @@ class SpinLock
     @buffer.rotate!(-1)
   end
 
+  def part2
+    @insert_at = 0
+    @next_char = 0
+    50000000.times { simulate }
+
+    @next_char
+  end
+
+  <<-docs
+    Simulate could be used to find the number that goes after 2017
+    in part1 as well, we'd just also need to track the location of
+    the number of interest (2017), which we don't need here since
+    we only care about 0.
+  docs
+  def simulate
+    p self if @i % 10000000 == 0
+    @i += 1
+    @insert_at = (@insert_at + @steps) % @i
+    @next_char = @i if @insert_at == 0
+    @insert_at += 1
+  end
 end
