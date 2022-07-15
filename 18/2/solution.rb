9c86636c4d0a0c267fbe233307fe952ff12532a1
Miller Arlindo Hall <hall@hey.com>
2022-07-15 15:51:24 -0400

    Problem 18/2 - Find strings that differ by one character

---
+++ 18/2/solution.rb
@@ -0,0 +1,338 @@
+
+Id = Struct.new(:text)
+
+class Id
+  def count(n)
+    text.chars.uniq.each { |ch|
+      return true if text.count(ch) == n
+    }
+
+    false
+  end
+
+  def distance(other)
+    different_indices(other).count
+  end
+
+  def different_indices(other)
+    text.chars
+      .each_with_index
+      .filter { |ch, i|
+        ch != other.text[i]
+      }
+      .lazy
+      .map(&:last)
+  end
+end
+
+class Warehouse
+  def initialize(box_ids)
+    @box_ids = box_ids
+  end
+
+  def self.of(text)
+    new(
+      text.strip.split("\n").map{|s| Id[s]}
+    )
+  end
+
+  def checksum
+    two_of_any.count * three_of_any.count
+  end
+
+  def two_of_any
+    box_ids.filter { |id|
+      id.count(2)
+    }
+  end
+
+  def three_of_any
+    box_ids.filter { |id|
+      id.count(3)
+    }
+  end
+
+  def box_ids(&block)
+    @box_ids.each(&block).lazy
+  end
+
+  def shared_between_correct
+    found, other = two_correct
+    index = found.different_indices(other).first
+
+    p [index, found, other]
+    found.text[0...index] + found.text[index+1..]
+  end
+
+  def two_correct
+    @box_ids.each { |id|
+      if found = @box_ids.filter { |other| id.distance(other) == 1 }.first
+        return [found, id]
+      end
+    }
+
+    nil
+  end
+end
+
+@example = <<-ids
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+ids
+
+@input = <<-ids
+lnfgdsywjyleogambzuchirkpx
+nnfqdskfjyteogambzuchirkpx
+lnfqdvvwjyteofambzuchirkpf
+lnfqdsvwjyteogvmbzuthirkpn
+ltfqdsvwjyoeogambxuchirkpx
+lnfqcsvwjytzogacbzuchirkpx
+lpfpdsvwjyteogambyuchirkpx
+pnfqdsvwjyteogqmbzuchinkpx
+lnfqdsvwjytyopambzpchirkpx
+lnfqisswjyteogadbzuchirkpx
+lnfqdsuwjcteogambzuchirepx
+lnfqdovwjnteigambzuchirkpx
+lnfbdsvwjxteogambzuchirkax
+lnfqdsawjyteogamxzuchiwkpx
+lncqdsvwjoteogambzuchirfpx
+lnfadsrwjyteogambzuchirktx
+lnfqdsvhjyteorazbzuchirkpx
+lwfqdsvwjytdogambzuchirkhx
+lnfqdhvwjyteogambzuhairkpx
+lnfqdsvwjytlogambzgchyrkpx
+lnfqdsvwjyteogamnzjwhirkpx
+lnfodsvwjyteogahuzuchirkpx
+lnfqdsvtjyteogamvzwchirkpx
+lnfqdsvwjzueogambzuxhirkpx
+lnfqxsvljytxogambzuchirkpx
+lnfqdsvwjyteogambpyvhirkpx
+lqzqdsvwjnteogambzuchirkpx
+lnfqdsvwjyteogumbzichirapx
+lnfqdbvwjytedgaubzuchirkpx
+lnfqdsvwpyteogabbhuchirkpx
+nnfqdsvwryteogambzuchiwkpx
+lrfqdsvwjeteogambzuchhrkpx
+lnfqdsvwxyteogamtzucyirkpx
+lnfsdsvwjyteogambzulhirknx
+lnfqdsvwjyreoyambzmchirkpx
+ltfqdsvwjytdogkmbzuchirkpx
+lnfqwbvcjyteogambzuchirkpx
+lnfqdsvwjyteogamrzfchirmpx
+lnfqdsvwjqteogambzucwirkpy
+lnfqdslwjyfeogambzuchikkpx
+lnfqdsvwjybeogambzuchikjpx
+lofqysvwjyteogasbzuchirkpx
+lnfqusvwjyteogambzucverkpx
+lnfqdsvwjyteogaibzuchfrkzx
+lnfqdsvwjyleogabbzuchirkcx
+lnfqdsvqjyteogambzuchdqkpx
+lnfqdsvwjwtewgambzuciirkpx
+lnfqisvwjatwogambzuchirkpx
+lnfqdgvwjyteogambzuchizkix
+lnfqdsxwjyteogambyuehirkpx
+lpffdsvwjyteogamrzuchirkpx
+lnirdsvwjyteogambzuchirkbx
+lnfqdsxdjyteogazbzuchirkpx
+lnfgdgvwyyteogambzuchirkpx
+lnfqxsvwjyteogambzmcwirkpx
+lnxqjsvwjyteogambzuchirkqx
+lnrqdsvwjpteogkmbzuchirkpx
+lnfqdstwjyteoeahbzuchirkpx
+lnfqdsvwtuteogambzuchixkpx
+lwfqvsvwjyteogambzughirkpx
+lnkqdsvwjyfeogambzuuhirkpx
+lvvqdsvwjyteogambzuchirkpn
+jndqdsvwjyteogzmbzuchirkpx
+enfqdszwjyteogambcuchirkpx
+lnfqdsvwiyteogakbauchirkpx
+lnfqdsvwjyteogtmbzxcdirkpx
+fnfqdswwjyteogawbzuchirkpx
+lnfqdsvwjydejqambzuchirkpx
+lnqqdsvwjyteogambzucbdrkpx
+lnfqdsvwjyteogadbzuchirxcx
+lnfqdslwjyyeogambzulhirkpx
+lnfqdsvwjytecgambzucgirkpb
+lbmqdsvwjyteogamkzuchirkpx
+lbfqdsvrjyteogambzuchirapx
+lnfqdsmwjyteogambzucfarkpx
+lnfqasvwoyteofambzuchirkpx
+bnfudsvwjyteogambzucharkpx
+lnfrdsvwjytxogambzuchirkpg
+lbfqdsvwjyteagambzucdirkpx
+lxfqdsvwjytuogambzucjirkpx
+lnfqdsvwjcteogamyzuchiikpx
+lnfodsvwjyteognmbzuchirapx
+ltfqdsvwjytedgaxbzuchirkpx
+lnfqdshwjyteogambzucsilkpx
+lnfqdsvwpyteohambzuchitkpx
+wnzqdsvwjyteogambiuchirkpx
+lnfqdsvwayteogambzhchirkpw
+ltfqcsvwjrteogambzuchirkpx
+lnfqdsvwaytekgamvzuchirkpx
+lnfqdsvwjyteogambzokpirkpx
+lnfqysbwjyeeogambzuchirkpx
+lnsqdsvwjyteogambzuchikkpd
+lrfqdsvwjyteogahbzochirkpx
+lnfqdsvwjyreoggmbzuchjrkpx
+lxfqdsvwjyteogkmbzuchirkpp
+enhqdbvwjyteogambzuchirkpx
+jnfqdsvwjyteogamczuuhirkpx
+lnfqdsvwuyteogadbzuchirkpw
+lnfqdsvjjytergambznchirkpx
+lnfqdsvwjyteoglmbzuceijkpx
+lwfqdsvwjyteogamieuchirkpx
+lnfqdsvwjfaeogambzqchirkpx
+lfbqdjvwjyteogambzuchirkpx
+lnfqdsvwjxteoaambzuchirkpp
+lnfqdsvwjyheogjmbzgchirkpx
+lnfqdskwjyteonambzuchiikpx
+lnfqdwvwjyteogambxuchirkph
+pnfqdsvwdyteogambzuchihkpx
+lnoqdsvwjyteogaybznchirkpx
+lnfqdsvwfyxefgambzuchirkpx
+lnfqdsvwjyteotamxzmchirkpx
+lnfqdsvwjyteigwmbzuchivkpx
+lnfqdsvwjytekgambcuchirkwx
+lnfqdsvwjuteogamrzulhirkpx
+lnfqdsvwjyteogambzucczrgpx
+wnfqzsvwjyteogambduchirkpx
+lnfqdsowjyteogambuuthirkpx
+lnfqdsvrjyteogcmbzuclirkpx
+knfqdsvwgyteogambzuchorkpx
+lnaqdsvwjytuogdmbzuchirkpx
+lnfrdsvwjyteogambluchigkpx
+lnfqqzvwjyteogambzkchirkpx
+lnfqdsvwjyteogamuzuchgrkux
+lnfqdsvnjyteogxmbznchirkpx
+lnfqdsvwjyteolajbzuchdrkpx
+lnfqdsvwjypeoiagbzuchirkpx
+lnrqdsvwjyteozamuzuchirkpx
+lnfqdsvwjytkogaubzucqirkpx
+lnkbdsvwjyteogacbzuchirkpx
+unfqdsvwjybeogambwuchirkpx
+lnfqfsvzjyteogambzuchiikpx
+lnfqdsvgjyreogahbzuchirkpx
+lnfqdsewjyteogavbeuchirkpx
+lnfqdsvwjdteogambbuchidkpx
+lnfqdsvwjythogambzcchirkvx
+lnfqdscwjyteorambzuchirgpx
+cnfqdzvwjyteogambzushirkpx
+lnfgdsgwjytedgambzuchirkpx
+lnfqdsvwbyteogimbzuchdrkpx
+lnfqdsswjyteogambzuohbrkpx
+lnfqdsvwjytqogabbzachirkpx
+lnfqdsvwjyteogmmbzucqiukpx
+lnfxdsrwjyteogambzuchnrkpx
+lnfqnqvwjyteogambzuchiwkpx
+lffqisvwjyteogambzulhirkpx
+lnfqdsxwjydeogambzucfirkpx
+lnfqdsvwjyteogambzucjirkrp
+lnfqdsnqjyteogambduchirkpx
+fnfqdmvwjyteogamlzuchirkpx
+lnfqvsvwjyteooamdzuchirkpx
+lnfqdsvcyyteogambzuchickpx
+onfqdsvwjyqeogambzuchirqpx
+znfqdcvwjyteoaambzuchirkpx
+lnfqdsvwjzteogambzuchidklx
+lnfqjsvwjyteogjmbzuchirkpv
+lnfqdsvwjytgorambzuchirppx
+lzfqdsvwpfteogambzuchirkpx
+lnfidsfwjyteogapbzuchirkpx
+lnfodsvwbyteobambzuchirkpx
+lnlqdsvwjytefgambzuchfrkpx
+lnkqdsvwjyteogambzkchgrkpx
+tnfqdsvwjyteoiamhzuchirkpx
+lnfqdsvwjyteogamllschirkpx
+lnfqdsvwjmthogamizuchirkpx
+lnfqdbvwjyteogafbzuchirkpb
+lnfxosvwjyteogahbzuchirkpx
+lnmqdsvwjyzeogambzuchirkcx
+lnfqdevbjytxogambzuchirkpx
+lnfqdsvwjyteogamzzudhipkpx
+lnfqdszwjyteoqambzuchirkpp
+lffqdsvwjyteogamtouchirkpx
+lnfqdsvhjytfogambzucharkpx
+hnfqdsvwjyteogembzschirkpx
+lnfqdsvwjateogambzuchirmpa
+lnfqdsvcjyteogambzocairkpx
+lnfqdsvwjyteogamwzmchirkpd
+lnfqzsvwjyteogdmbzuyhirkpx
+lnfqdsvwjytfyglmbzuchirkpx
+lnfndsvwjyteogambzuchirktf
+gnfqdnvwjytevgambzuchirkpx
+lnfqdsvwjyteoganbpuchorkpx
+lnfpdsvwnyteogambzucqirkpx
+fnfqdstejyteogambzuchirkpx
+lnfqlsvwjyteowambzuchirkmx
+lnfqdsvwjyteogmmdzuchtrkpx
+lnfqdsvwcyteogaqbzuchirkqx
+lnfqdsvwjytlogtmbzuchiwkpx
+lnfqdsvwoyteogambzuczirkwx
+lnfqdsvwjyteogzybzucdirkpx
+lnfqdvvwjyteogumbzuchiukpx
+lnfqbwvwjyteogambzuchjrkpx
+lnfgdsvwjyteogambzvchirkzx
+lnfqdsvwjvtjogambzuchiokpx
+lnfedsvwjyteogambzuchivkph
+lhfqusvwjytaogambzuchirkpx
+lnfqdsvwjyteogacbzuihirkpv
+lnfwdsvwjyteogambzucokrkpx
+lnfqtsvwjpteognmbzuchirkpx
+anfqdswwjyteogambzucairkpx
+lnfqdsvwjyteorambzuchirlsx
+lnfqdsvwjytgogambzychirkpc
+lnfqdhvwjyteogambzachirklx
+lnfwdsvwjyteogaobquchirkpx
+rnfqdsvwjiteogambzuhhirkpx
+lnfqdsuwjyemogambzuchirkpx
+hnfqdsvwjyteogambzuchprfpx
+anfqssvwjyteogambzumhirkpx
+lnfkdsvwjyteogafbzqchirkpx
+lnfqdsvwjyteogacqzuchirspx
+lnfqdskwjyteggambzuchiakpx
+lnnqdsvwjyteooambzuchihkpx
+lnlqdsvjjyteogambzuchgrkpx
+lnfqdsvwjyteogamszochirkex
+lnfqbsvwjyteogambzqchirepx
+lnfqdsbwjcteogambzhchirkpx
+lnfqdwvzjyteogambzechirkpx
+ynfadsvwdyteogambzuchirkpx
+tnfqdsvwjytuogambzuohirkpx
+lnfqdsvwjyteogambzaohivkpx
+mnfqisvwjyteogagbzuchirkpx
+lnfqbsvwjyueogambzuchirkhx
+ynfqdsvwjyteogdmbzuchinkpx
+lnfqdwhwjyteogambzuchirqpx
+mnfqdsvwjyteogambzfchkrkpx
+lnfqdsnwjyteogambzgchiqkpx
+lnfqdsvwjytergambzuchiuklx
+lnfqdqvjjyteogamtzuchirkpx
+lnfqdscwjyteorambzuchzrgpx
+enfqdevwjyteogaabzuchirkpx
+gnfqdsvbjyteogambzuchirkph
+lnfqdxvwjyteogambzubhixkpx
+lnfqdsvwjyteogambojchihkpx
+lnfqdsvwjytdogambzuzhilkpx
+lnfqdsvwjyteogamezuqhirtpx
+tnfhdsvwjyteogambzuvhirkpx
+lnfzdsvwjnteogahbzuchirkpx
+lnfqdsvwjyteogambzfzhirkvx
+lnfqqsvwjyteogambzuchirgpo
+lufqpsvwjythogambzuchirkpx
+lnfqdsvwjyteogzmbzuchimkix
+lnwqdspwjyteogambzcchirkpx
+lnfqdsowjyteogambzuchigypx
+lnfqdnvvjyteogambzucjirkpx
+lnfjdsvwryteogambzuchirkcx
+lnfqdsvwbyteogambzuchirfpb
+lnfqdsvwjyheogambzxchprkpx
+lnfqmsvwjytezgambzuchirlpx
+lnaqdsvwjyteogamdzuzhirkpx
+lnoqdsvwjytebgambfuchirkpx
+lnfqdtvwjytvogambzuchirkpv
+ids
