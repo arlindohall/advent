0688c7f628dc336918b3cfb94faf964c15ca786c
Miller Arlindo Hall <hall@hey.com>
2022-07-19 14:26:30 -0400

    Use linked list to represent circle
    This version finishes in 26 seconds rather than what was at least a few minutes
    for the previous version (it never actually finished running for me).

--- 18/9/solution.rb
+++ 18/9/solution.rb
@@ -1,6 +1,57 @@
 
 $debug = false
 
+class LinkedListQueue
+
+  Node = Struct.new(:value, :next, :prev)
+
+  attr_reader :head
+
+  def initialize(val)
+    @head = Node[val]
+    @head.next = @head
+    @head.prev = @head
+  end
+
+  def unshift(val)
+    n = Node[val, @head, @head.prev]
+    @head.prev.next = n
+    @head.prev = n
+    @head = n
+  end
+
+  def shift
+    n = @head
+    n.prev.next = n.next
+    n.next.prev = n.prev
+
+    @head = n.next
+    n.value
+  end
+
+  def rotate!(n)
+    if n > 0
+      n.times { @head = @head.next }
+    elsif n < 0
+      n.abs.times { @head = @head.prev }
+    end
+  end
+
+  def to_s
+    to_a.inspect
+  end
+
+  def to_a
+    return [] if @head.nil?
+    ptr, a = @head.next, [@head.value]
+    until ptr == @head
+      a << ptr.value
+      ptr = ptr.next
+    end
+    a
+  end
+end
+
 class MarbleGame
   def initialize(players, marbles)
     @players = Array.new(players, 0)
@@ -8,7 +59,7 @@ class MarbleGame
   end
 
   def play
-    @turn, @player, @circle = 1, 0, [0]
+    @turn, @player, @circle = 1, 0, LinkedListQueue.new(0)
     until @turn > @marbles
       debug
       place_marble
@@ -19,7 +70,7 @@ class MarbleGame
   end
 
   def debug
-    puts "[#{(@player + 1).to_s}]".ljust(4) + "#{@circle}" if $debug
+    puts "[#{(@player + 1).to_s}]".ljust(6) + "#{@circle.to_a}" if $debug
   end
 
   def place_marble
