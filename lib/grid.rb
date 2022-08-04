d8cb292dd5b5e8a338cd4edf82b50862741b1f9c
Miller Arlindo Hall <hall@hey.com>
2022-08-04 17:27:53 -0400

    Problem 18/20 - Broken because of inefficient building whole graph

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,17 +1,5 @@
 
-class Grid
-  def initialize(grid)
-    @grid = grid
-  end
-
-  def self.parse(text)
-    new(text.strip.split("\n").map(&:chars))
-  end
-
-  def to_s
-    @grid.map(&:join).join("\n")
-  end
-
+module Coordinates
   def adjacent(x, y)
     [
       [x+1, y],
@@ -32,6 +20,43 @@ class Grid
       [x, y+1],
       [x, y-1],
     ].filter { |x,y| in_bounds?(x, y) }
+  end
+
+  def corners(x, y)
+    [
+      [x+1, y-1],
+      [x+1, y+1],
+      [x-1, y-1],
+      [x-1, y+1],
+    ].filter { |x,y| in_bounds?(x, y) }
+  end
+
+  def in_bounds(x, y)
+    raise "Not implemented for class #{self.class}"
+  end
+end
+
+class SparseGrid
+  include Coordinates
+
+  def in_bounds?(x, y)
+    true
+  end
+end
+
+class Grid
+  include Coordinates
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def self.parse(text)
+    new(text.strip.split("\n").map(&:chars))
+  end
+
+  def to_s
+    @grid.map(&:join).join("\n")
   end
 
   def in_bounds?(x, y)
