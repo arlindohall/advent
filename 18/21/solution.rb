03479781f45501cc8c28fdc2810ea315c48b7b1c
Miller Arlindo Hall <hall@hey.com>
2022-08-07 11:08:58 -0400

    Instead of de-compiling, recognize the relevant instruction

--- 18/21/solution.rb
+++ 18/21/solution.rb
@@ -79,7 +79,7 @@ class Watch
   end
 
   def _apply
-    puts [@registers[0], @registers[4]].inspect if @ip == 29
+    look_for_cycle
     value = case @instruction.name
     when :addr
       register(@instruction.a) + register(@instruction.b)
@@ -122,6 +122,19 @@ class Watch
     @ip = @registers[@ip_reg] + 1
   end
 
+  def look_for_cycle
+    return if @ip != 29
+
+    reg0 = @registers[0]
+    reg4 = @registers[4]
+    @seen ||= []
+
+    raise "Found cycle: #{@seen.last}" if @seen.include?(reg4)
+
+    @seen << reg4
+    puts "r0=#{reg0}, r4=#{reg4}, cycles=#{@seen.size} (check=#{@seen.uniq.size})"
+  end
+
   def register(i)
     @registers[i]
   end
@@ -132,97 +145,20 @@ class Watch
   end
 end
 
-def run(value = 1_000_000_000_000)
+def run(value = 1)
   Watch.parse(@example).with_register_0(value).execute
 end
 
-# The analyses below don't really solve the problem, instead I just
-# added a debug print of the regs 0 and 4 when the ip is 29 so I can
-# see what value will escape the loop. I made a mistake in the decompilation.
+=begin
+The key to the solution is recognizing that the only jump instruction that
+exits the loop is on line 29, so you can just print the value of registers
+0 and 4 when the IP is at that value. Then to solve, just call `run(1)` or
+really `run` with any value that doesn't halt, and see what happens.
 
+For part 2, just keep track of the values in the registers, and the first
+time there's a cycle, print the one before. (runs in ~8min with truffleruby)
+=end
 
-# - : #ip 3                     ; <- set ip to track r3
-# 0 : seti 123 0 4              ; r4 := 123
-# 1 : bani 4 456 4              ; r4 &= 456
-# 2 : eqri 4 72 4               ; r4 := r4 == 72 ? 1 : 0
-# 3 : addr 4 3 3                ; jump_if (r4 == 1) 4 else 5
-# 4 : seti 0 0 3                ; jump 1
-# 5 : seti 0 6 4                ; r4 := 0
-# 6 : bori 4 65536 1            ; r1 := r4 | 65536 (0x1_00_00)
-# 7 : seti 678134 1 4           ; r4 := 678134 (0xa_58_f6)
-# 8 : bani 1 255 5              ; r5 := r1 & 255
-# 9 : addr 4 5 4                ; r4 += r5
-# 10: bani 4 16777215 4         ; r4 &= 16777215 (0xff_ff_ff)
-# 11: muli 4 65899 4            ; r4 *= 65899 (0x1016b)
-# 12: bani 4 16777215 4         ; r4 &= 16777215 (0xff_ff_ff)
-# 13: gtir 256 1 5              ; r5 := 256 > r1 ? 1 : 0
-# 14: addr 5 3 3                ; jump_if (r5 == 1) 16 else 15
-# 15: addi 3 1 3                ; jump 17
-# 16: seti 27 8 3               ; jump 28
-# 17: seti 0 1 5                ; r5 := 0
-# 18: addi 5 1 2                ; r2 := r5 + 1
-# 19: muli 2 256 2              ; r2 *= 256                         <- r2 = r2 << 8
-# 20: gtrr 2 1 2                ; r2 = r2 > r1 ? 1 : 0
-# 21: addr 2 3 3                ; jump_if (r2 == 1) 23 else 22
-# 22: addi 3 1 3                ; jump 24
-# 23: seti 25 7 3               ; jump 26
-# 24: addi 5 1 5                ; r5 += 1
-# 25: seti 17 1 3               ; jump 18
-# 26: setr 5 3 1                ; r1 := r5
-# 27: seti 7 8 3                ; jump 8
-# 28: eqrr 4 0 5                ; r5 = r4 == r0 ? 1 : 0
-# 29: addr 5 3 3                ; jump_if (r5 == 1) 31 else 30
-# 30: seti 5 4 3                ; jump 6
-
-#   - : <- set ip to track r3
-#   5 : r4 := 0
-# x 6 : r1 := r4 | 65536 (0x1_00_00)
-#   7 : r4 := 678134 (0xa_58_f6)
-# x 8 : r5 := r1 & 255
-#   9 : r4 += r5
-#   10: r4 &= 16777215 (0xff_ff_ff)
-#   11: r4 *= 65899 (0x1016b)
-#   12: r4 &= 16777215 (0xff_ff_ff)
-#   13: r5 := 256 > r1 ? 1 : 0
-#   14: jump_if (r5 == 1) 16 else 15 <- get to 16 from here if r1 < 256, which is always true first time, line 8 sets to 255
-# x 15: jump 17
-# x 16: jump 28 <- get to escape clause here
-# x 17: r5 := 0
-# x 18: r2 := r5 + 1
-#   19: r2 *= 256                         <- r2 = r2 << 8
-#   20: r2 = r2 > r1 ? 1 : 0
-#   21: jump_if (r2 == 1) 23 else 22
-# x 22: jump 24
-# x 23: jump 26
-# x 24: r5 += 1
-#   25: jump 18
-# x 26: r1 := r5
-#   27: jump 8
-# x 28: r5 = r4 == r0 ? 1 : 0
-#   29: jump_if (r5 == 1) 31 else 30 <- escape here if r4 == r0
-# x 30: jump 6
-
-#   - : <- set ip to track r3
-#   5 : r4 := 0
-# x 6 : loop do
-#         r1 := r4 | 65536
-#   7 :   r4 := 678134
-# x 8 :   loop do 
-#           r5 := r1 & 255
-#   12:     r4 := (((r4 + r5) & 16777215) * 65899) & 16777215
-# x 16:     halt! if (256 > r1) && (r4 == r0)
-# x 17:     r1 := r1 * 256
-#   27:   end
-# x 30: end
-
-
-# dead code?
-# x 18:     do
-#   19:       r2 := (r5 + 1) * 256
-#   21:       if (r2 <= r1)
-# x 24:         r5 += 1 ; end
-# x 26:     until (r2 > r1) ; r1 := r5
-  
 @example = <<-inst.strip
 #ip 3
 seti 123 0 4
