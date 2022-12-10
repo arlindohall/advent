38a601d75021ef68b835065fcd9943582649e636
Miller Arlindo Hall <hall@hey.com>
2022-12-10 14:40:15 -0500

    Update helper to print example and input files

--- lib/input.rb
+++ lib/input.rb
@@ -2,9 +2,9 @@
 require_relative './helpers'
 
 def read_input
-  Problem.current.input_file.read.strip
+  @input ||= Problem.current.input_file.read.strip
 end
 
 def read_example
-  Problem.current.example_file.read.strip
+  @example ||= Problem.current.example_file.read.strip
 end
