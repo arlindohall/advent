16eb1bb39575268be7cb271d16451b072c9421d8
Miller Arlindo Hall <hall@hey.com>
2022-12-28 17:17:27 -0500

    Update helper and setup script to autocomplete open

--- advent.fish
+++ advent.fish
@@ -7,5 +7,5 @@ end
 
 if ! complete -c advent-helper | grep advent-helper
   complete advent-helper \
-    -a "current current-input current-example next repl input day create example help"
+    -a "current open code current-input current-example next repl input day create example help"
 end
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "To reset the helper autocomplete, run: 'complete -e advent-helper'"
 cp ./advent.fish $HOME/.config/fish/conf.d/
