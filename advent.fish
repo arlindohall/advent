490794e6fb4a71183745eb35165f4d3be061a0f9
Miller Arlindo Hall <hall@hey.com>
2022-12-30 12:49:25 -0500

    Errors in helpers

--- advent.fish
+++ advent.fish
@@ -1,10 +1,4 @@
 
-if ! test -f /usr/local/bin/advent-helper
-  echo '#!/bin/bash
-  ./helper $@
-  ' > /usr/local/bin/advent-helper
-end
-
 if ! complete -c advent-helper | grep advent-helper
   complete advent-helper \
     -a "current open code current-input current-example next repl input day create example help"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
 echo "To reset the helper autocomplete, run: 'complete -e advent-helper'"
-cp ./advent.fish $HOME/.config/fish/conf.d/
+
+function install-helper {
+  if ! test -f /usr/local/bin/advent-helper ; then
+    sudo touch /usr/local/bin/advent-helper
+    sudo chown "$USER" /usr/local/bin/advent-helper
+    sudo chmod u+x /usr/local/bin/advent-helper
+    echo '#!/bin/bash
+    ./helper $@
+    ' > /usr/local/bin/advent-helper
+  fi
+}
+
+function install-fish-helper {
+  cp ./advent.fish "$HOME"/.config/fish/conf.d/
+}
+
+function setup {
+  install-helper
+  install-fish-helper
+}
+
+setup
