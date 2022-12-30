9ab8c7be709fb85962f9bfe5fb16a27c90c6e2b4
Miller Arlindo Hall <hall@hey.com>
2022-12-30 13:14:11 -0500

    Simplify command autocomplete in helper

--- advent.fish
+++ advent.fish
@@ -1,5 +1,7 @@
 
-if ! complete -c advent-helper | grep advent-helper
-  complete advent-helper \
-    -a "current open code current-input current-example next repl input day create example help"
+set advent_commands "current open code current-input current-example next repl input day create example"
+if ! complete -c advent-helper | grep $advent_commands
+  complete -e advent-helper
+  complete -c advent-helper -f -a $advent_commands
 end
+set advent_commands
--- setup.sh
+++ setup.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "To reset the helper autocomplete, run: 'complete -e advent-helper'"
-
 function install-helper {
-  if ! test -f /usr/local/bin/advent-helper ; then
-    sudo touch /usr/local/bin/advent-helper
-    sudo chown "$USER" /usr/local/bin/advent-helper
-    sudo chmod u+x /usr/local/bin/advent-helper
-    echo '#!/bin/bash
-    ./helper $@
-    ' > /usr/local/bin/advent-helper
-  fi
+  sudo touch /usr/local/bin/advent-helper
+  sudo chown "$USER" /usr/local/bin/advent-helper
+  sudo chmod u+x /usr/local/bin/advent-helper
+  echo '#!/bin/bash
+  ./helper $@
+  ' > /usr/local/bin/advent-helper
 }
 
 function install-fish-helper {
