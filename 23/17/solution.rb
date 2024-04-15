fcce2041cd8a7513e5b2240a1686f749def3a198
Miller Arlindo Hall <hall@hey.com>
2024-04-15 09:12:47 -0400

    Problem 23/17 Stolen from todd.ginsberg.com because it was stupid hard

--- 23/17/solution.rb
+++ 23/17/solution.rb
@@ -21,40 +21,28 @@ class CrucibleMap
 
     until queue.empty?
       current = queue.pop
-      if current.loc_direc.location == [map.first.size - 1, map.size - 1]
-        return current.heat_loss
-      end
+      return current.heat_loss if current.loc_direc.location == goal
 
       [[1, 0], [0, 1], [-1, 0], [0, -1]].reject do |direction|
-          direction == [0, 1] && current.loc_direc.direction == [0, -1] ||
-            direction == [0, -1] && current.loc_direc.direction == [0, 1] ||
-            direction == [1, 0] && current.loc_direc.direction == [-1, 0] ||
-            direction == [-1, 0] && current.loc_direc.direction == [1, 0]
+          backtracking?(current, direction)
         end
         .filter do |direction|
-          dx, dy = direction
-          x, y = current.loc_direc.location
-          x + dx >= 0 && x + dx < map.first.size && y + dy >= 0 &&
-            y + dy < map.size
+          in_bounds?(current.loc_direc.next_location(direction))
         end
         .filter do |direction|
           distance = current.loc_direc.distance + 1
           distance < 3 || current.loc_direc.direction != direction
         end
         .map do |direction|
-          dx, dy = direction
-          x, y = current.loc_direc.location
-          distance =
-            (
-              if current.loc_direc.direction == direction
-                current.loc_direc.distance + 1
-              else
-                0
-              end
-            )
+          next_location = current.loc_direc.next_location(direction)
+          distance = 0
+          if current.loc_direc.direction == direction
+            distance = current.loc_direc.distance + 1
+          end
+
           LocDirecHeatLoss[
-            LocDirec[[dx + x, dy + y], direction, distance],
-            current.heat_loss + map[dy + y][dx + x]
+            LocDirec[next_location, direction, distance],
+            current.heat_loss + map[next_location.second][next_location.first]
           ]
         end
         .reject { |state| seen.include?(state.loc_direc) }
@@ -67,6 +55,80 @@ class CrucibleMap
     raise "No path found"
   end
 
-  LocDirec = Struct.new(:location, :direction, :distance)
+  def part_2
+    seen = Set.new
+    queue = PriorityQueue.new { |state| -state.heat_loss } # PQ pops the max key
+
+    [
+      LocDirecHeatLoss[LocDirec[[0, 0], [1, 0], 0], 0],
+      LocDirecHeatLoss[LocDirec[[0, 0], [0, 1], 0], 0]
+    ].each do |state|
+      queue << state
+      seen << state.loc_direc
+    end
+
+    until queue.empty?
+      current = queue.pop
+      if current.loc_direc.location == goal &&
+           current.loc_direc.distance >= 4 && current.loc_direc.distance < 10
+        return current.heat_loss
+      end
+
+      [[1, 0], [0, 1], [-1, 0], [0, -1]].reject do |direction|
+          backtracking?(current, direction)
+        end
+        .filter do |direction|
+          in_bounds?(current.loc_direc.next_location(direction))
+        end
+        .filter do |direction|
+          distance = current.loc_direc.distance + 1
+          (distance < 4 && current.loc_direc.direction == direction) ||
+            (distance >= 10 && current.loc_direc.direction != direction) ||
+            (distance >= 4 && distance < 10)
+        end
+        .map do |direction|
+          next_location = current.loc_direc.next_location(direction)
+          distance = 0
+          if current.loc_direc.direction == direction
+            distance = current.loc_direc.distance + 1
+          end
+
+          LocDirecHeatLoss[
+            LocDirec[next_location, direction, distance],
+            current.heat_loss + map[next_location.second][next_location.first]
+          ]
+        end
+        .reject { |state| seen.include?(state.loc_direc) }
+        .each do |state|
+          seen << state.loc_direc
+          queue << state
+        end
+    end
+
+    raise "No path found"
+  end
+
+  def goal
+    [map.first.size - 1, map.size - 1]
+  end
+
+  def in_bounds?((x, y))
+    x >= 0 && x < map.first.size && y >= 0 && y < map.size
+  end
+
+  def backtracking?(current, direction)
+    direction == [0, 1] && current.loc_direc.direction == [0, -1] ||
+      direction == [0, -1] && current.loc_direc.direction == [0, 1] ||
+      direction == [1, 0] && current.loc_direc.direction == [-1, 0] ||
+      direction == [-1, 0] && current.loc_direc.direction == [1, 0]
+  end
+
   LocDirecHeatLoss = Struct.new(:loc_direc, :heat_loss)
+  LocDirec =
+    Struct.new(:location, :direction, :distance) do
+      def next_location((dx, dy))
+        x, y = location
+        [dx + x, dy + y]
+      end
+    end
 end
