6829f051991c5acaec39e6345573826b8252a735
Miller Arlindo Hall <hall@hey.com>
2024-04-15 19:51:21 -0400

    Refactor so that dijsktra's is shared

--- 23/17/solution.rb
+++ 23/17/solution.rb
@@ -7,21 +7,21 @@ class CrucibleMap
     @text.split("\n").map { |line| line.split("").map(&:to_i) }
   end
 
-  def part_1
+  def part_1 = dijkstra(Small)
+  def part_2 = dijkstra(Large)
+  def dijkstra(crucible = Small)
     seen = Set.new
     queue = PriorityQueue.new { |state| -state.heat_loss } # PQ pops the max key
 
-    [
-      LocDirecHeatLoss[LocDirec[[0, 0], [1, 0], 0], 0],
-      LocDirecHeatLoss[LocDirec[[0, 0], [0, 1], 0], 0]
-    ].each do |state|
+    [[1, 0], [0, 1]].each do |dir|
+      state = LocDirecHeatLoss[LocDirec[[0, 0], dir, 0], 0]
       queue << state
       seen << state.loc_direc
     end
 
     until queue.empty?
       current = queue.pop
-      return current.heat_loss if current.loc_direc.location == goal
+      return current.heat_loss if crucible.win?(current, goal)
 
       [[1, 0], [0, 1], [-1, 0], [0, -1]].reject do |direction|
           backtracking?(current, direction)
@@ -29,22 +29,8 @@ class CrucibleMap
         .filter do |direction|
           in_bounds?(current.loc_direc.next_location(direction))
         end
-        .filter do |direction|
-          distance = current.loc_direc.distance + 1
-          distance < 3 || current.loc_direc.direction != direction
-        end
-        .map do |direction|
-          next_location = current.loc_direc.next_location(direction)
-          distance = 0
-          if current.loc_direc.direction == direction
-            distance = current.loc_direc.distance + 1
-          end
-
-          LocDirecHeatLoss[
-            LocDirec[next_location, direction, distance],
-            current.heat_loss + map[next_location.second][next_location.first]
-          ]
-        end
+        .filter { |direction| crucible.valid_turn?(current, direction) }
+        .map { |direction| current.follow(direction, map) }
         .reject { |state| seen.include?(state.loc_direc) }
         .each do |state|
           seen << state.loc_direc
@@ -55,75 +41,30 @@ class CrucibleMap
     raise "No path found"
   end
 
-  def part_2
-    seen = Set.new
-    queue = PriorityQueue.new { |state| -state.heat_loss } # PQ pops the max key
-
-    [
-      LocDirecHeatLoss[LocDirec[[0, 0], [1, 0], 0], 0],
-      LocDirecHeatLoss[LocDirec[[0, 0], [0, 1], 0], 0]
-    ].each do |state|
-      queue << state
-      seen << state.loc_direc
-    end
-
-    until queue.empty?
-      current = queue.pop
-      if current.loc_direc.location == goal &&
-           current.loc_direc.distance >= 4 && current.loc_direc.distance < 10
-        return current.heat_loss
-      end
-
-      [[1, 0], [0, 1], [-1, 0], [0, -1]].reject do |direction|
-          backtracking?(current, direction)
-        end
-        .filter do |direction|
-          in_bounds?(current.loc_direc.next_location(direction))
-        end
-        .filter do |direction|
-          distance = current.loc_direc.distance + 1
-          (distance < 4 && current.loc_direc.direction == direction) ||
-            (distance >= 10 && current.loc_direc.direction != direction) ||
-            (distance >= 4 && distance < 10)
-        end
-        .map do |direction|
-          next_location = current.loc_direc.next_location(direction)
-          distance = 0
-          if current.loc_direc.direction == direction
-            distance = current.loc_direc.distance + 1
-          end
-
-          LocDirecHeatLoss[
-            LocDirec[next_location, direction, distance],
-            current.heat_loss + map[next_location.second][next_location.first]
-          ]
-        end
-        .reject { |state| seen.include?(state.loc_direc) }
-        .each do |state|
-          seen << state.loc_direc
-          queue << state
-        end
-    end
-
-    raise "No path found"
-  end
-
-  def goal
-    [map.first.size - 1, map.size - 1]
-  end
-
+  def goal = [map.first.size - 1, map.size - 1]
   def in_bounds?((x, y))
     x >= 0 && x < map.first.size && y >= 0 && y < map.size
   end
 
-  def backtracking?(current, direction)
-    direction == [0, 1] && current.loc_direc.direction == [0, -1] ||
-      direction == [0, -1] && current.loc_direc.direction == [0, 1] ||
-      direction == [1, 0] && current.loc_direc.direction == [-1, 0] ||
-      direction == [-1, 0] && current.loc_direc.direction == [1, 0]
+  def backtracking?(current, (dx, dy))
+    cx, cy = current.loc_direc.direction
+    dx == -cx && dy == -cy
   end
 
-  LocDirecHeatLoss = Struct.new(:loc_direc, :heat_loss)
+  LocDirecHeatLoss =
+    Struct.new(:loc_direc, :heat_loss) do
+      def follow(direction, map)
+        next_location = loc_direc.next_location(direction)
+        distance = 0
+        distance = loc_direc.distance + 1 if loc_direc.direction == direction
+
+        LocDirecHeatLoss[
+          LocDirec[next_location, direction, distance],
+          heat_loss + map[next_location.second][next_location.first]
+        ]
+      end
+    end
+
   LocDirec =
     Struct.new(:location, :direction, :distance) do
       def next_location((dx, dy))
@@ -131,4 +72,29 @@ class CrucibleMap
         [dx + x, dy + y]
       end
     end
+
+  class Small
+    def self.win?(current, goal)
+      current.loc_direc.location == goal
+    end
+
+    def self.valid_turn?(current, direction)
+      distance = current.loc_direc.distance + 1
+      distance < 3 || current.loc_direc.direction != direction
+    end
+  end
+
+  class Large
+    def self.win?(current, goal)
+      current.loc_direc.location == goal && current.loc_direc.distance >= 4 &&
+        current.loc_direc.distance < 10
+    end
+
+    def self.valid_turn?(current, direction)
+      distance = current.loc_direc.distance + 1
+      (distance < 4 && current.loc_direc.direction == direction) ||
+        (distance >= 10 && current.loc_direc.direction != direction) ||
+        (distance >= 4 && distance < 10)
+    end
+  end
 end
